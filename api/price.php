c932f1442ad4bfd780b23302a229afb25c8df1c5
= <=>
2021-01-30 01:12:01 +0600

    Can Calculate Non airport to airport

--- api/price.php
+++ api/price.php
@@ -4,17 +4,19 @@ namespace AAALRouteMapHelper;
 
 
 function show_price(){
+
     if(isset($_GET['from']) && isset($_GET['to'])){
 
        $_from = $_GET['from'];
        $_to = $_GET['to'];
        
-       echo get_price_text($_from, $_to);
+       $price_string = get_price_text($_from, $_to);
+       $basic_car_price_arr = string_to_price($price_string);
+       
+    //    $prices_for_everycar = get_prices_for_everycar($basic_car_price_arr);
+       get_prices_for_everycar($basic_car_price_arr);
 
-       // print_r(get_basic_price($_from, $_to));
-
-       // get_column_name_for_airport("Heathrow Airport");
-        
+       
 
     }else{
         echo \json_encode(['message' => 'Please select from and to location.']);
@@ -59,11 +61,73 @@ function get_price_text($from, $to){
  * 
  *
  */
-// make from to price detection
 
+// input S-45/R-80
+// return ['s'=> 30, 'r' => 20]
+function string_to_price($price_string){
+    $string_price_arr = \explode('/', $price_string);
+    
+    $pattern = '/[^\d.]*/';
+    $single_price = \preg_replace($pattern, '', $string_price_arr[0]);
+    $return_price = \preg_replace($pattern, '', $string_price_arr[1]);
+
+    // return key name as s and r instead off single and return 
+    // so I won't misspell
+    return [ 's' => $single_price, 'r' => $return_price ];
+}
 
 // return price for all four cars
+// Input ['s' => 1, 'r' => 2]
 // return [car1 => [s=>50, r=>100], car2 => ..]
+function get_prices_for_everycar($basic_car_price){
+    // Note: Price will be added as following
+    // Salon = $basic_car_price
+    // Estate Car: S- £6/ R-12 . 
+    // MPV: S-£15/R-£30. 
+    // Executive: S-£15/ R-£30	
+    $cars_and_addtions = [
+        'Salon' => [
+                's' => 0,
+                'r' => 0,
+        ],
+        'Estate' => [
+                's' => 6,
+                'r' => 12,
+        ],
+        'MPV' => [
+                's' => 15,
+                'r' => 30,
+        ],
+        'Executive Saloon' => [
+                's' => 15,
+                'r' => 30,
+        ],
+    ];
 
-// s-12/r-30 devide and return pricing
-// return ['s'=> 30, 'r' => 20]; as input S-45/R-80
+    $prices_for_everycar = [];
+
+    foreach(array_keys($cars_and_addtions) as $car_name){
+        
+        $prices_for_everycar[$car_name] = [
+            // I'm adding this car price
+            // With Basic Car Price
+            's' => $basic_car_price['s']+$cars_and_addtions[$car_name]['s'],
+            'r' => $basic_car_price['r']+$cars_and_addtions[$car_name]['r'],
+        ];
+
+    }
+
+    echo "<PRE>";
+    print_r($prices_for_everycar);
+    echo "</PRE>";
+
+    
+}
+
+    // echo "BASIC CAR <BR>";
+    // \print_r($basic_car_price);
+
+
+
+
+// reverse from to if needed.
